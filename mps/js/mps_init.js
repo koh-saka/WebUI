3a752ce4b327513aa409e19d9276c153e54cd7da
Koichiro Sakamoto <sakamoto.k.096@gmail.com>
2026-04-06 15:53:09 +0900

    A-003-1 全体の主要コンテナ配置完了

--- mps/js/mps_init.js
+++ mps/js/mps_init.js
@@ -13,10 +13,7 @@ var MpsApp = (function () {
     let ui = {
         title: null,
         backBtn: null,
-        viewTop: null,
-        viewBottom: null,
-        panelHeader: null,
-        viewPlaceholder: null
+        status: null
     };
 
     function init() {
@@ -38,10 +35,6 @@ var MpsApp = (function () {
         ui.title = TextCtrl("mps_header", "mps_title", state.title);
         ui.backBtn = ButtonCtrl("mps_header", "mps_back_btn", "Back", callbackBack);
 
-        ui.viewTop = TextCtrl("mps_view_top", "mps_view_placeholder", "LIVE VIEW AREA");
-        ui.viewBottom = TextCtrl("mps_view_bottom", "mps_view_placeholder", "BOTTOM CONTROL AREA");
-
-        ui.panelHeader = TextCtrl("mps_panel_header", "mps_panel_placeholder", "PANEL HEADER");
         ui.status = TextCtrl("mps_panel_body", "mps_status", state.message);
     }
 
@@ -50,10 +43,9 @@ var MpsApp = (function () {
     }
 
     function load() {
-        console.log("load");
-
         updateView();
         checkLayout();
+        updateDebugInfo();
     }
 
     function updateView() {
@@ -61,12 +53,6 @@ var MpsApp = (function () {
 
         ui.title.show();
         ui.backBtn.show();
-
-        ui.viewTop.show();
-        ui.viewBottom.show();
-
-        ui.panelHeader.show();
-
         ui.backBtn.displayOff();
 
         ui.status.set(state.message);
@@ -87,17 +73,51 @@ var MpsApp = (function () {
         console.log("===== Layout Check =====");
 
         const ids = [
+            "#mps_root",
+            "#mps_header",
+            "#mps_main",
             "#mps_view",
             "#mps_view_top",
             "#mps_view_bottom",
             "#mps_panel",
             "#mps_panel_header",
-            "#mps_panel_body"
-        ];
+            "#mps_panel_body",
 
+            ".mps_live_view_container",
+            ".mps_live_view_header",
+            ".mps_live_view_body",
+            ".mps_live_view_split",
+            ".mps_live_view_side_controls",
+
+            ".mps_preset_container",
+            ".mps_preset_toolbar",
+            ".mps_preset_body",
+            ".mps_preset_list_container",
+            ".mps_preset_center_line",
+            ".mps_target_frame_container",
+
+            ".mps_manual_control_container",
+            ".mps_limitation_container",
+            ".mps_track_adjust_tabs_container",
+            ".mps_auto_framing_container",
+            ".mps_image_adjust_container"
+        ];
+        
         ids.forEach(id => {
             const el = $(id);
             console.log(id, el.length ? "OK" : "NG");
+        });
+    }
+
+    function updateDebugInfo() {
+        document.querySelectorAll('#mps_root .debug-box').forEach(function (el) {
+            const rect = el.getBoundingClientRect();
+            el.setAttribute(
+                'title',
+                el.getAttribute('data-name') + ' : ' +
+                Math.round(rect.width) + ' x ' +
+                Math.round(rect.height)
+            );
         });
     }
 
